b5e5e2498d13b8324d16f396174a0ef9a61437da
Nyame100 <nyamerichmond100@gmail.com>
2022-07-05 20:14:31 +0200

    display team data into frontend

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from pages.models import Team
+
 # Register your models here.
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name',
+                    'designation', 'photo', 'date_created']
